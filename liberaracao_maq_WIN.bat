ed98a497386d58ddaad5f53f12e9bf9ff8b4a9ab
Paulo César <91353178+Paulo-CSA@users.noreply.github.com>
2026-04-27 11:15:28 -0300

    Add files via upload

--- liberaracao_maq_WIN.bat
+++ liberaracao_maq_WIN.bat
@@ -1,19 +1,14 @@
 @echo off
 echo ==== FIX COMPLETO PsExec ====
 
-:: 1. Garantir usuario admin ativo e com senha
-net user admin admin /active:yes
-
-:: 2. Colocar no grupo Administradores
-net localgroup Administrators admin /add
-
-:: 3. Liberar UAC remoto
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
 
-:: 4. Habilitar compartilhamentos administrativos
+netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=Yes
+
+netsh advfirewall set allprofiles state off
+
 reg add HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters /v AutoShareWks /t REG_DWORD /d 1 /f
 
-:: 5. Iniciar serviços essenciais
 sc config LanmanServer start= auto
 sc start LanmanServer
 
@@ -23,33 +18,3 @@ sc start LanmanWorkstation
 sc config RemoteRegistry start= auto
 sc start RemoteRegistry
 
-:: 6. Firewall OFF (teste)
-netsh advfirewall set allprofiles state off
-
-:: 7. Criar politica de seguranca completa
-echo [Unicode] > %temp%\psexec_full.inf
-echo Unicode=yes >> %temp%\psexec_full.inf
-echo [Version] >> %temp%\psexec_full.inf
-echo signature="$CHICAGO$" >> %temp%\psexec_full.inf
-echo Revision=1 >> %temp%\psexec_full.inf
-echo [Privilege Rights] >> %temp%\psexec_full.inf
-
-:: Permissoes importantes
-echo SeBatchLogonRight = *S-1-5-32-544 >> %temp%\psexec_full.inf
-echo SeServiceLogonRight = *S-1-5-32-544 >> %temp%\psexec_full.inf
-echo SeNetworkLogonRight = *S-1-5-32-544 >> %temp%\psexec_full.inf
-
-:: Limpar negacoes
-echo SeDenyBatchLogonRight = >> %temp%\psexec_full.inf
-echo SeDenyNetworkLogonRight = >> %temp%\psexec_full.inf
-echo SeDenyServiceLogonRight = >> %temp%\psexec_full.inf
-
-:: Aplicar politica
-secedit /configure /db %temp%\psexec_full.sdb /cfg %temp%\psexec_full.inf /areas USER_RIGHTS
-
-:: 8. Atualizar politicas
-gpupdate /force
-
-echo ==== FINALIZADO ====
-echo Reinicie o computador antes de testar!
-pause
